3c3699c5f90651614627ab266e2636522c4e6569
Cristian Mauricio Leiton Valencia <crileiton@umariana.edu.co>
2019-07-27 14:21:09 -0500

    Add files via upload

---
+++ android/app/src/main/kotlin/com/crileiton/flutter_app_crileiton/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.crileiton.flutter_app_crileiton
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
